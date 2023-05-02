48c2dfd52d51cccc8b2bd117b7db306b36869cd3
RisheekK <86208506+RisheekK@users.noreply.github.com>
2023-05-02 02:24:20 -0400

    added new html file for 401 - unauthorized

--- Project/main.py
+++ Project/main.py
@@ -14,6 +14,9 @@ print(CURR_DIR)
 sys.path.append(CURR_DIR)
 
 # custom error pages
+def unauthorized(e):
+    return render_template('401.html'), 401
+
 def page_not_found(e):
     return render_template('404.html'), 404
 
@@ -25,6 +28,7 @@ login_manager = flask_login.LoginManager()
 # app = Flask(__name__)
 def create_app(config_filename=''):
     app = Flask(__name__)
+    app.register_error_handler(401, unauthorized)
     app.register_error_handler(404, page_not_found)
     app.register_error_handler(403, permission_denied)
     app.secret_key = os.environ.get("SECRET_KEY", "missing_secret")
